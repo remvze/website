a722eaabc1e7ed686d06fff2a91c1bbfa6a678e5
MAZE <remvze@proton.me>
2022-12-24 06:57:31 -0800

    feat: add basic home view

---
+++ src/components/home-view/home-view.jsx
@@ -0,0 +1,11 @@
+import styles from './home-view.module.css';
+
+const HomeView = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default HomeView;
