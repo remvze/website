536f982aae7526680d9b7be55cced36a1b3f1b9b
MAZE <remvze@proton.me>
2022-12-24 07:39:36 -0800

    feat: add basic structure

--- src/components/home-view/home-view.jsx
+++ src/components/home-view/home-view.jsx
@@ -6,7 +6,29 @@ const HomeView = () => {
   return (
     <div className={styles.wrapper}>
       <Container>
-        <h1>Hello World</h1>
+        <h1 className={styles.title}>
+          <p>
+            <span>✱ Developer &</span>
+            <br />
+            <span>
+              <i>Design</i> Technologist.
+            </span>
+          </p>
+        </h1>
+
+        <p className={styles.desc}>
+          A design-driven develop<span className={styles.orange}>✺</span>by day,
+          dedicated to crafting practical solutions with delightful experiences
+          through simple yet elegant design and scalable code. A curiour mind
+          <span className={styles.indigo}>✶</span>by night, philosophizing day
+          to day life in order to make sense of it all and eventually
+          <span className={styles.green}>✣</span>connect the dots.
+        </p>
+
+        <p className={styles.desc}>
+          Currently the founder of{' '}
+          <a href="https://instagram.com/philosophors">✧ Philosophors</a>.
+        </p>
       </Container>
     </div>
   );
