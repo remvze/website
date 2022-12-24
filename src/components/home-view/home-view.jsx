966e38aad861067a9abdfe240a3c0765908ccd65
MAZE <remvze@proton.me>
2022-12-24 12:38:08 -0800

    style: add emoji animation

--- src/components/home-view/home-view.jsx
+++ src/components/home-view/home-view.jsx
@@ -144,7 +144,7 @@ const HomeView = () => {
         </motion.p>
 
         <motion.h3 variants={variants} className={styles.label}>
-          Let&apos;s connect 🤙
+          Let&apos;s connect <span className={styles.callEmoji}>🤙</span>
         </motion.h3>
 
         <div>
