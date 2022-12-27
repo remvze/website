9906aafb35ccd17ea05b7e6ec5b3e6032764376e
MAZE <remvze@proton.me>
2022-12-27 05:16:38 -0800

    feat: implement base project card

--- src/components/projects-view/projects-view.jsx
+++ src/components/projects-view/projects-view.jsx
@@ -1,5 +1,49 @@
+import { motion } from 'framer-motion';
+
+import Container from '@/components/container';
+
+import styles from './projects-view.module.css';
+
 const ProjectsView = () => {
-  return <h1>Projects</h1>;
+  const projects = [
+    {
+      name: 'Moice',
+      desc: 'Minimal and intuitive task manager.',
+      link: 'https://moice.run',
+      logo: '/images/moice.svg',
+    },
+    {
+      name: 'Pice',
+      desc: 'The password generator you need.',
+      link: 'https://moice.run',
+      logo: '/images/moice.svg',
+    },
+  ];
+
+  return (
+    <div className={styles.wrapper}>
+      <Container>
+        <motion.h1 className={styles.title}>Projects</motion.h1>
+        <p className={styles.desc}>
+          A collection of carefully crafted products.
+        </p>
+
+        <div className={styles.projects}>
+          {projects.map((project, i) => (
+            <div className={styles.projectContainer} key={i}>
+              <div className={styles.projectLogo}>
+                <img src={project.logo} alt="logo" />
+              </div>
+              <div className={styles.projectInfo}>
+                <h3 className={styles.projectName}>{project.name}</h3>
+                <p className={styles.projectDesc}>{project.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default ProjectsView;
