0d29f8ce2c7ba67028a067561a22be680a726aaf
MAZE <remvze@proton.me>
2022-12-27 06:59:11 -0800

    style: add animations

--- src/components/projects-view/projects-view.jsx
+++ src/components/projects-view/projects-view.jsx
@@ -20,17 +20,51 @@ const ProjectsView = () => {
     },
   ];
 
+  const wrapperVariants = {
+    hide: {},
+    show: {
+      transition: {
+        delayChildren: 0.4,
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const variants = {
+    hide: {
+      opacity: 0,
+      x: -30,
+      y: 5,
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      y: 0,
+    },
+  };
+
   return (
-    <div className={styles.wrapper}>
+    <motion.div
+      variants={wrapperVariants}
+      initial="hide"
+      animate="show"
+      className={styles.wrapper}
+    >
       <Container>
-        <motion.h1 className={styles.title}>Projects</motion.h1>
-        <p className={styles.desc}>
+        <motion.h1 variants={variants} className={styles.title}>
+          Projects
+        </motion.h1>
+        <motion.p variants={variants} className={styles.desc}>
           A collection of carefully crafted products.
-        </p>
+        </motion.p>
 
         <div className={styles.projects}>
           {projects.map((project, i) => (
-            <div className={styles.projectContainer} key={i}>
+            <motion.div
+              variants={variants}
+              className={styles.projectContainer}
+              key={i}
+            >
               <div className={styles.projectLogo}>
                 <img src={project.logo} alt="logo" />
               </div>
@@ -40,20 +74,19 @@ const ProjectsView = () => {
                 {project.link ? (
                   <a className={styles.projectLink} href={project.link}>
                     <div className={styles.visit}>
-                      <span>Visit</span>
-                      <span>Visit</span>
-                    </div>{' '}
-                    <span className={styles.arrow}>→</span>
+                      <span>Visit Project</span>
+                      <span>Visit Project</span>
+                    </div>
                   </a>
                 ) : (
                   <p className={styles.comingSoon}>Coming Soon</p>
                 )}
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </Container>
-    </div>
+    </motion.div>
   );
 };
 
