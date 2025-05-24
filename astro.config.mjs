45908b4f185a6cd570d7197e3d5a04a3843a14e9
MAZE <remvze@proton.me>
2025-05-24 22:14:56 +0330

    chore: change some things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 import sitemap from '@astrojs/sitemap';
 
@@ -7,6 +9,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   integrations: [react(), sitemap()],
   markdown: {
+    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkMath],
     shikiConfig: {
       theme: 'github-dark',
     },
