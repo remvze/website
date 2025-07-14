518fce57d04650c611b421e8debf711865045def
MAZE <remvze@proton.me>
2025-07-14 21:58:15 +0330

    feat: add bookmarks page

---
+++ src/data/bookmarks.ts
@@ -0,0 +1,13 @@
+export const bookmarks: Array<{
+  date: string;
+  title: string;
+  type: 'video' | 'article' | 'other';
+  url: string;
+}> = [
+  {
+    date: '2025-07-14',
+    title: 'Readme Driven Development',
+    type: 'article',
+    url: 'https://tom.preston-werner.com/2010/08/23/readme-driven-development',
+  },
+];
