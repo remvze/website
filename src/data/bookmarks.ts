d3f75561223fa2b15022af646a7a0b00508f63aa
MAZE <remvze@proton.me>
2025-07-14 22:01:39 +0330

    feat: minor improvements

--- src/data/bookmarks.ts
+++ src/data/bookmarks.ts
@@ -1,4 +1,11 @@
-export const bookmarks: Array<{
+import { reverseArray } from '@/helpers/array';
+
+/**
+ * From Old to New
+ * YYYY-MM-DD
+ */
+
+const _bookmarks: Array<{
   date: string;
   title: string;
   type: 'video' | 'article' | 'other';
@@ -10,4 +17,12 @@ export const bookmarks: Array<{
     type: 'article',
     url: 'https://tom.preston-werner.com/2010/08/23/readme-driven-development',
   },
+  {
+    date: '2025-07-14',
+    title: "Conway's Law",
+    type: 'article',
+    url: 'https://en.wikipedia.org/wiki/Conway%27s_law',
+  },
 ];
+
+export const bookmarks = reverseArray(_bookmarks);
