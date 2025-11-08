d2717985bf42a71d6835d68ce1fcda4f76f7714f
MAZE <remvze@proton.me>
2025-11-08 13:38:08 +0330

    feat: remake

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  markdown: {
-    rehypePlugins: [rehypeKatex],
-    remarkPlugins: [remarkMath],
-    shikiConfig: {
-      theme: 'github-dark',
-    },
-  },
-  site: 'https://progress.rip',
+  site: 'https://remvze.com',
 });
